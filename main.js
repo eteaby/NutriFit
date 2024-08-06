a4a0acfb7a2553866c477566fd04cfd7354ebda3
eteaby <samrawitfikremariam@gmail.com>
2024-08-06 22:52:47 +0300

    nutrifit front

---
+++ main.js
@@ -0,0 +1,16 @@
+
+const navMenu= document.getElementById('nav-menu');
+const closeIcon= document.getElementById('nav-close');
+const navLink= document.querySelectorAll('nav-link');
+
+navLink.forEach(link=>
+    link.addEventListener('click',()=>{
+        navMenu.classList.add('hidden')
+    }) 
+)
+closeIcon.addEventListener('click',()=>{
+    navMenu.classList.add('hidden')
+})
+hamburger.addEventListener('click', ()=>{
+    navMenu.classList.remove('hidden');
+})
